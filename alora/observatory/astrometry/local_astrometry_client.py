6ccea65984b52b5f2e662b016bd735610ee424be
Alora Observatory <tmo.observer@gmail.com>
2024-12-24 08:44:09 -0800

    perform extraction server side actually

--- alora/observatory/astrometry/local_astrometry_client.py
+++ alora/observatory/astrometry/local_astrometry_client.py
@@ -10,37 +10,29 @@ from alora.astroutils import calc_mean_fwhm, source_catalog
 from alora.config import config
 from alora.observatory.interfaces import PlateSolve
 
+
+acfg = config["ASTROMETRY"]
+
 class Astrometry(PlateSolve):
     def solve(self,impath, *args, **kwargs):
-        self.write_out(f"Solving {impath}...")
         with fits.open(impath) as hdul:
             header = hdul[0].header
             guess_coords = SkyCoord(Angle(header["OBJCTRA"],unit="hourangle"), Angle(header["OBJCTDEC"],unit="deg"))
-            scale = config["CAMERA"]["FIELD_WIDTH"]  # arcmin
+            scale = config["CAMERA"]["FIELD_WIDTH"] # arcmin
 
-        return solve(impath, guess_coords=guess_coords, scale=scale, scale_units="arcminwidth", *args, write_out=self.write_out **kwargs)
+        resp = solve(impath, guess_coords=guess_coords, scale=scale, scale_units="arcminwidth", *args, write_out=self.write_out, **kwargs)
+        return resp.json()["success"], resp.content
 
-def main():
-    path = sys.argv[1]
-    ast = Astrometry()
-    print(ast.solve(path).content)
-
-def make_source_cat(data:np.ndarray):
-    mean_fwhm = calc_mean_fwhm(data)
-    cat = source_catalog(data,source_sigma=3,ncont=5,fwhm_pix=mean_fwhm)
-    return cat["xcentroid","ycentroid"]
-
-def solve(path, guess_coords:SkyCoord=None, scale:Quantity=config["CAMERA"]["PIX_SCALE"]*u.arcsec, *args, **kwargs,scale_units="arcsecperpix"):
+def solve(path, guess_coords:SkyCoord=None, scale:float=config["CAMERA"]["PIX_SCALE"], scale_units="arcsecperpix", *args, **kwargs):
     if guess_coords is not None:
         pass
         kwargs["ra"] = guess_coords.ra.deg
         kwargs["dec"] = guess_coords.dec.deg
         kwargs["radius"] = config["ASTROMETRY"]["SEARCH_RADIUS"]
     if scale is not None:
-        kwargs["scale-low"] = scale.to_value("arcsec")*0.95
-        kwargs["scale-high"] = scale.to_value("arcsec")*1.05
         kwargs["scale-units"] = scale_units
-
+        kwargs["scale-low"] = scale*0.95
+        kwargs["scale-high"] = scale*1.05
     return _solve(path, *args, **kwargs)
 
 def gensolve(path, guess_ra=None, guess_dec=None, scale_lower=None, scale_upper=None, *args, **kwargs):
@@ -55,28 +47,10 @@ def gensolve(path, guess_ra=None, guess_dec=None, scale_lower=None, scale_upper=
 
     return _solve(path, *args, **kwargs)
 
-def _solve(filepath, *args, **kwargs,write_out=print):
-    # kwargs["filepath"] = abspath(path)
+def _solve(filepath,write_out=print,extract=True, *args, **kwargs):
+    kwargs["filepath"] = abspath(filepath)
     write_out(f"Solving {filepath}...")
-    with fits.open(filepath) as hdul:
-        data = hdul[0].data
 
-    write_out("Making source catalog...")
-    cat = make_source_cat(data)
-    if ".fits" in filepath:
-        newpath = filepath.replace(".fits", "_cat.fits")
-    else:
-        newpath = filepath.replace(".fit", "_cat.fits")
-
-    cat.write(newpath, format='fits', overwrite=True)
-    # print("cat path:",newpath)
-
-    kwargs["width"] = data.shape[1]
-    kwargs["height"] = data.shape[0]
-    kwargs["x-column"] = "xcentroid"
-    kwargs["y-column"] = "ycentroid"
-    kwargs["fitspath"] = filepath  # specify the fits path that the sol will be written into, even tho the actual file the solution is derived from is a catalog
-    kwargs["filepath"] = newpath
 
     kwargs["tweak-order"] = 2
     # kwargs["objs"] = 1000
@@ -84,7 +58,16 @@ def _solve(filepath, *args, **kwargs,write_out=print):
     # kwargs["nsigma"]=5
     kwargs["flags"] = list(args) + ["--no-plots", "--crpix-center", "--overwrite", "-v"]
     write_out("Sending solve request...")
-    return requests.post("http://localhost:5555/solve", json=kwargs, headers={"Content-Type": "application/json"})
+
+    endpoint = "cat_and_solve" if extract else "solve"
+
+    return requests.post(f"http://localhost:{acfg['PORT']}/{endpoint}", json=kwargs, headers={"Content-Type": "application/json"})
+
+
+def main():
+    path = sys.argv[1]
+    ast = Astrometry()
+    print(ast.solve(path))
 
 if __name__ == "__main__":
     main()
--- alora/observatory/astrometry/local_astrometry_server.py
+++ alora/observatory/astrometry/local_astrometry_server.py
@@ -1,5 +1,5 @@
 import os
-from os.path import join, dirname, abspath
+from os.path import join, dirname, abspath, basename, splitext
 import subprocess
 from flask import Flask, request, jsonify
 from astropy.io import fits
@@ -9,6 +9,18 @@ import astropy.units as u
 import numpy as np
 import tomlkit
 import sqlite3
+from alora.astroutils import calc_mean_fwhm, source_catalog
+from alora.config import config
+
+import warnings
+warnings.filterwarnings('ignore', module="astropy.io.fits.card")
+warnings.filterwarnings('ignore', module="astropy.io.fits.convenience")
+warnings.filterwarnings('ignore', module="photutils.segmentation.deblend")
+
+def make_source_cat(data:np.ndarray):
+    mean_fwhm = calc_mean_fwhm(data)
+    cat = source_catalog(data,source_sigma=3,ncont=5,fwhm_pix=mean_fwhm)
+    return cat["xcentroid","ycentroid"]
 
 db_path = join(dirname(abspath(__file__)), "astrom.db")
 conn = sqlite3.connect(db_path)
@@ -17,9 +29,8 @@ cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS astrometry (id INTEGER PRIMARY KEY AUTOINCREMENT, filepath TEXT, wcs TEXT, flags TEXT, status TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
 conn.commit()
 conn.close()
-config_path = join(dirname(abspath(__file__)),"config.toml")
-with open(config_path,"rb") as f:
-    config = tomlkit.load(f)
+
+acfg = config["ASTROMETRY"]
 
 def connect_to_db():
     conn = sqlite3.connect(db_path)
@@ -30,7 +41,7 @@ def connect_to_db():
 logging_dir = abspath(join(dirname(abspath(__file__)),"logs"))
 
 os.makedirs(logging_dir,exist_ok=True)
-os.makedirs(config["SOLVE_DIR"],exist_ok=True)
+os.makedirs(acfg["SOLVE_DIR"],exist_ok=True)
 
 app = Flask(__name__)
 
@@ -55,36 +66,49 @@ def mark_failed(record_id):
     conn.commit()
     conn.close()
 
+# TODO: make this more rigorous (not just C and D drives lol)
+def wpath_to_wsl(windows_path):
+    return windows_path.replace("D:\\", "/mnt/d/").replace("C:\\", "/mnt/c/").replace("\\","/")
+
 def _solve(data):
-    print(data["filepath"])
-    filepath = data.get('filepath').replace("D:\\", "/mnt/d/").replace("C:\\", "/mnt/c/").replace("\\","/")
-    print(filepath)
-    flags = data.get('flags', [])
-
-    if not filepath or not os.path.isfile(filepath):
-        return jsonify({'error': f"Invalid file path '{filepath}'", "job_id":-1}), 400
-    
-    wcspath = join(config["SOLVE_DIR"], filepath.replace(".fits", ".wcs"))
-    data["wcs"] = wcspath
-
-    args = ['solve-field', filepath] + flags
-    for key in data:
-        if key not in ['filepath',"flags","fitspath"]:
-            args.append(f'--{key}')
-            args.append(str(data[key]))
-        print(f"{key}: {data[key]}")
-    print(args)
-    job_id = create_record(filepath,wcspath," ".join(args))
+    job_id = -1
     try:
-        result = subprocess.run(args, capture_output=True, text=True)
+        filepath = data["filepath"]
+        print(filepath)
+        flags = data.get('flags', [])
+
+        if not filepath or not os.path.isfile(filepath):
+            return jsonify({'error': f"Invalid file path '{filepath}'", "job_id":-1,"success":False}), 400
+        
+        solve_dir = abspath(acfg["SOLVE_DIR"])
+
+        wcspath = join(solve_dir, basename(splitext(filepath)[0]+".wcs"))
+        data["wcs"] = wpath_to_wsl(wcspath)
+
+        # solpath is a path to a file that astrometry will create to indicate that solving succeeded
+        solpath = join(solve_dir,basename(splitext(filepath)[0]+".solved"))
+        data["solved"] = wpath_to_wsl(solpath)
+        
+        data["dir"] = wpath_to_wsl(solve_dir)
+
+        args = ["wsl","solve-field",wpath_to_wsl(filepath)] + flags
+        for key in data:
+            if key not in ['filepath',"flags","fitspath"]:
+                args.append(f'--{key}')
+                args.append(str(data[key]))
+            print(f"{key}: {data[key]}")
+        
+        job_id = create_record(filepath,wcspath," ".join(args))
+        print(args)
+        result = subprocess.run(args=args, capture_output=True, text=True)
         if result.returncode != 0:
             mark_failed(job_id)
-            return jsonify({'astrometry error': result.stderr, "job_id":job_id}), 500
+            return jsonify({'astrometry error': result.stderr, "job_id":job_id,"success":False}), 500
         
         with fits.open(wcspath) as hdul:
             wcs_header = hdul[0].header
 
-        fitspath = data.get("fitspath",filepath).replace("D:\\", "/mnt/d/").replace("C:\\", "/mnt/c/").replace("\\","/")
+        fitspath = data.get("fitspath",filepath)
 
         with fits.open(fitspath, mode='update') as hdul:
             header = hdul[0].header
@@ -92,15 +116,55 @@ def _solve(data):
                 header[key] = wcs_header[key]
         with open(join(logging_dir,f"astrom_{job_id}.log"),"w+") as f:
             f.write(result.stdout)
-        print(f"Solved {filepath}")
-        mark_solved(job_id)
-        return jsonify({'message': 'Astrometry.net processing complete', "job_id":job_id}), 200
+        if os.path.exists(solpath):
+            print(f"Solved {filepath}")
+            mark_solved(job_id)
+            return jsonify({'message': 'Astrometry.net processing complete', "job_id":job_id, "success":True}), 200
+        else:
+            mark_failed(job_id)
+            print(f"Failed to solve {filepath}")
+            return jsonify({'message': 'Astrometry.net did not find a solution', "job_id":job_id, "success":False}), 201
+
     except Exception as e:
-        return jsonify({'error': str(e),"job_id":job_id}), 500
+        if job_id != -1:
+            mark_failed(job_id)
+        return jsonify({'error': str(e),"job_id":job_id,"success":False}), 500
 
 @app.route('/solve', methods=['POST'])
 def solve():
     return _solve(request.get_json())
+
+@app.route("/cat_and_solve",methods=["POST"])
+def cat_and_solve():
+    req = request.get_json()
+    filepath = req["filepath"]
+    print(filepath)
+    # flags = req.get('flags', [])
+
+    if not filepath or not os.path.isfile(filepath):
+        return jsonify({'error': f"Invalid file path '{filepath}'", "job_id":-1,"success":False}), 400
+
+    with fits.open(filepath) as hdul:
+        data = hdul[0].data
+
+    print("Making source catalog...")
+    cat = make_source_cat(data)
+    if ".fits" in filepath:
+        newpath = filepath.replace(".fits", "_cat.fits")
+    else:
+        newpath = filepath.replace(".fit", "_cat.fits")
+
+    cat.write(newpath, format='fits', overwrite=True)
+    # print("cat path:",newpath)
+
+    req["width"] = data.shape[1]
+    req["height"] = data.shape[0]
+    req["x-column"] = "xcentroid"
+    req["y-column"] = "ycentroid"
+    req["fitspath"] = filepath  # specify the fits path that the sol will be written into, even tho the actual file the solution is derived from is a catalog
+    req["filepath"] = newpath
+    return _solve(req)
+
 
 @app.route('/status', methods=['POST'])
 def status():
@@ -140,4 +204,4 @@ def jobs():
     return jsonify({"jobs":[dict(r) for r  in records]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=config["PORT"])
+    app.run(host='0.0.0.0', port=acfg["PORT"],debug=True)
