56145d46ec805b983cec2cb66d150a0dcf8cad70
sagesanto <chellster1003@gmail.com>
2024-12-23 19:19:17 -0500

    astrom client fixes

--- alora/observatory/astrometry/local_astrometry_client.py
+++ alora/observatory/astrometry/local_astrometry_client.py
@@ -6,27 +6,28 @@ from astropy.coordinates import SkyCoord, Angle
 from astropy.units import Quantity
 import astropy.units as u
 
-from alora.config import config
+from alora.observatory.config import config
 
 path = sys.argv[1]
 
 def fancy_solve(path, *args, **kwargs):
     with fits.open(path) as hdul:
         header = hdul[0].header
-        guess_coords = SkyCoord(Angle(header["OBJCTRA"]), Angle(header["OBJCTDEC"],unit="deg"))
-        scale = config["CAMERA"]["PIX_SCALE"]*u.arcsec
+        guess_coords = SkyCoord(Angle(header["OBJCTRA"],unit="hourangle"), Angle(header["OBJCTDEC"],unit="deg"))
+        scale = config["CAMERA"]["PIX_SCALE"]*u.arcsec*header["XBINNING"]
 
     return solve(path, guess_coords=guess_coords, scale=scale, *args, **kwargs)
 
 def solve(path, guess_coords:SkyCoord=None, scale:Quantity=config["CAMERA"]["PIX_SCALE"]*u.arcsec, *args, **kwargs):
     if guess_coords is not None:
+        pass
         kwargs["ra"] = guess_coords.ra.deg
         kwargs["dec"] = guess_coords.dec.deg
-        kwargs["radius"] = config["ASTROMETRY"]["RADIUS"]
+        kwargs["radius"] = config["ASTROMETRY"]["SEARCH_RADIUS"]
     if scale is not None:
-        kwargs["scale_lower"] = scale.to_value("arcsec")
-        kwargs["scale_upper"] = scale.to_value("arcsec")
-        kwargs["scale_units"] = "arcsecperpix"
+        kwargs["scale-low"] = scale.to_value("arcsec")*0.95
+        kwargs["scale-high"] = scale.to_value("arcsec")*1.05
+        kwargs["scale-units"] = "arcsecperpix"
 
     return _solve(path, *args, **kwargs)
 
@@ -44,8 +45,12 @@ def gensolve(path, guess_ra=None, guess_dec=None, scale_lower=None, scale_upper=
 
 def _solve(path, *args, **kwargs):
     kwargs["filepath"] = path
-    kwargs["flags"] = args + ["--no-plots", "--crpix-center", "--overwrite", "--fits-image"]
+    kwargs["tweak-order"] = 2
+    # kwargs["objs"] = 1000
+    # kwargs["uniformize"] = 0
+    # kwargs["nsigma"]=5
+    kwargs["flags"] = list(args) + ["--no-plots", "--crpix-center", "--overwrite", "-v"]
     return requests.post("http://localhost:5555/solve", json=kwargs, headers={"Content-Type": "application/json"})\
 
 if __name__ == "__main__":
-    fancy_solve(path)
+    print(fancy_solve(path).content)
